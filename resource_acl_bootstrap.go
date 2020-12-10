86ca3e5868adc346945a47dfe5e1098e2e25952d
Adrienne Cohea <adriennecohea@gmail.com>
2020-12-09 22:19:41 -0800

    Make all values computed. Change error message

--- resource_acl_bootstrap.go
+++ resource_acl_bootstrap.go
@@ -31,29 +31,27 @@ func aclBootstrap() *schema.Resource {
 
 			"name": {
 				Description: "Human-readable name for this token.",
-				Optional:    true,
+				Computed:    true,
 				Type:        schema.TypeString,
 			},
 
 			"type": {
 				Description: "The type of token to create, 'client' or 'management'.",
-				Required:    true,
+				Computed:    true,
 				Type:        schema.TypeString,
 			},
 
 			"policies": {
 				Description: "The ACL policies to associate with the token, if it's a 'client' type.",
-				Optional:    true,
+				Computed:    true,
 				Type:        schema.TypeSet,
 				Elem:        &schema.Schema{Type: schema.TypeString},
 			},
 
 			"global": {
 				Description: "Whether the token should be replicated to all regions or not.",
-				Optional:    true,
+				Computed:    true,
 				Type:        schema.TypeBool,
-				ForceNew:    true,
-				Default:     false,
 			},
 
 			"create_time": {
@@ -71,7 +69,7 @@ func bootstrapACLs(d *schema.ResourceData, meta interface{}) error {
 	log.Println("[DEBUG] Bootstrapping ACL system")
 	resp, _, err := client.ACLTokens().Bootstrap(nil)
 	if err != nil {
-		return fmt.Errorf("error bootstrapping ACL system: %s", err.Error())
+		return fmt.Errorf("unable to bootstrap ACL system: %s", err.Error())
 	}
 	log.Printf("[DEBUG] Created ACL token %q", resp.AccessorID)
 	d.SetId(resp.AccessorID)
