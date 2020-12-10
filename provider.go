58099b1c5314c96f906fdf41e4a85e251d85bcd8
Adrienne Cohea <adriennecohea@gmail.com>
2020-12-09 22:05:33 -0800

    Basic resource

--- provider.go
+++ provider.go
@@ -1,14 +1,61 @@
 package main
 
 import (
+	"github.com/hashicorp/nomad/api"
 	"github.com/hashicorp/terraform/helper/schema"
 )
 
 //Provider defines the schema and resource map
 func Provider() *schema.Provider {
 	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"address": {
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("NOMAD_ADDR", "http://127.0.0.1:4646"),
+				Description: "URL of the root of the target Nomad agent.",
+			},
+			"ca_file": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("NOMAD_CACERT", ""),
+				Description: "A path to a PEM-encoded certificate authority used to verify the remote agent's certificate.",
+			},
+			"cert_file": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("NOMAD_CLIENT_CERT", ""),
+				Description: "A path to a PEM-encoded certificate provided to the remote agent; requires use of key_file.",
+			},
+			"key_file": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("NOMAD_CLIENT_KEY", ""),
+				Description: "A path to a PEM-encoded private key, required if cert_file is specified.",
+			},
+			"tls_server_name": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("NOMAD_CLIENT_KEY", ""),
+				Description: "Specifies an optional string used to set the SNI host when connecting to Vault via TLS.",
+			},
+		},
+
+		ConfigureFunc: providerConfigure,
+
 		ResourcesMap: map[string]*schema.Resource{
 			"nomadutility_acl_bootstrap": aclBootstrap(),
 		},
 	}
+}
+
+func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+	conf := api.DefaultConfig()
+	conf.Address = d.Get("address").(string)
+	conf.TLSConfig.CACert = d.Get("ca_file").(string)
+	conf.TLSConfig.ClientCert = d.Get("cert_file").(string)
+	conf.TLSConfig.ClientKey = d.Get("key_file").(string)
+	conf.TLSConfig.TLSServerName = d.Get("tls_server_name").(string)
+
+	return api.NewClient(conf)
 }
--- resource_acl_bootstrap.go
+++ resource_acl_bootstrap.go
@@ -1,37 +1,97 @@
 package main
 
-import "github.com/hashicorp/terraform/helper/schema"
+import (
+	"fmt"
+	"log"
+
+	"github.com/hashicorp/nomad/api"
+	"github.com/hashicorp/terraform/helper/schema"
+)
 
 func aclBootstrap() *schema.Resource {
 	return &schema.Resource{
-		Create: resourceServerCreate,
-		Read:   resourceServerRead,
-		Update: resourceServerUpdate,
-		Delete: resourceServerDelete,
+		Create: bootstrapACLs,
+		Read:   doNothing,
+		Update: doNothing,
+		Delete: forget,
+
 		Schema: map[string]*schema.Schema{
-			"address": {
-				Type:     schema.TypeString,
-				Default:  "127.0.0.1",
-				Optional: true,
+			"accessor_id": {
+				Description: "Nomad-generated ID for this token.",
+				Computed:    true,
+				Type:        schema.TypeString,
+			},
+
+			"secret_id": {
+				Description: "The value that grants access to Nomad.",
+				Computed:    true,
+				Sensitive:   true,
+				Type:        schema.TypeString,
+			},
+
+			"name": {
+				Description: "Human-readable name for this token.",
+				Optional:    true,
+				Type:        schema.TypeString,
+			},
+
+			"type": {
+				Description: "The type of token to create, 'client' or 'management'.",
+				Required:    true,
+				Type:        schema.TypeString,
+			},
+
+			"policies": {
+				Description: "The ACL policies to associate with the token, if it's a 'client' type.",
+				Optional:    true,
+				Type:        schema.TypeSet,
+				Elem:        &schema.Schema{Type: schema.TypeString},
+			},
+
+			"global": {
+				Description: "Whether the token should be replicated to all regions or not.",
+				Optional:    true,
+				Type:        schema.TypeBool,
+				ForceNew:    true,
+				Default:     false,
+			},
+
+			"create_time": {
+				Description: "The timestamp the token was created.",
+				Type:        schema.TypeString,
+				Computed:    true,
 			},
 		},
 	}
 }
 
-func resourceServerCreate(d *schema.ResourceData, m interface{}) error {
-	d.SetId("token-from-api")
+func bootstrapACLs(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*api.Client)
+
+	log.Println("[DEBUG] Bootstrapping ACL system")
+	resp, _, err := client.ACLTokens().Bootstrap(nil)
+	if err != nil {
+		return fmt.Errorf("error bootstrapping ACL system: %s", err.Error())
+	}
+	log.Printf("[DEBUG] Created ACL token %q", resp.AccessorID)
+	d.SetId(resp.AccessorID)
+
+	d.Set("accessor_id", resp.AccessorID)
+	d.Set("secret_id", resp.SecretID)
+	d.Set("name", resp.Name)
+	d.Set("type", resp.Type)
+	d.Set("policies", resp.Policies)
+	d.Set("global", resp.Global)
+	d.Set("create_time", resp.CreateTime.UTC().String())
+
 	return nil
 }
 
-func resourceServerRead(d *schema.ResourceData, m interface{}) error {
+func forget(d *schema.ResourceData, m interface{}) error {
 	d.SetId("")
 	return nil
 }
 
-func resourceServerUpdate(d *schema.ResourceData, m interface{}) error {
-	return nil
-}
-
-func resourceServerDelete(d *schema.ResourceData, m interface{}) error {
+func doNothing(d *schema.ResourceData, m interface{}) error {
 	return nil
 }
